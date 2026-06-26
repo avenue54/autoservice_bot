452025ffe4c65b93bf7875959709c8efc60def09
avenue54 <shashutin.tx@gmail.com>
2026-06-27 00:53:38 +0300

    Add cancel button and phone validation

--- bot.py
+++ bot.py
@@ -5,19 +5,23 @@ from aiogram.client.session.aiohttp import AiohttpSession
 from config import BOT_TOKEN
 from aiogram.filters import Command
 from handlers import router
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
 
 
 session = AiohttpSession(proxy="http://127.0.0.1:10809")
 bot = Bot(token=BOT_TOKEN, session=session)
 dp = Dispatcher()
 
+button = KeyboardButton(text="📝 Оставить заявку")
+keyboard = ReplyKeyboardMarkup(keyboard=[[button]], resize_keyboard=True)
+
 @dp.message(Command("start"))  # ← реагирует ТОЛЬКО на /start
 async def cmd_start(message):
     await message.answer(
         "Добро пожаловать в автосервис! 🚗\n\n"
         "Я помогу вам оставить заявку на ремонт.\n"
-        "Напишите /request чтобы начать."
-    )
+        "Нажмите на кнопку ниже, чтобы начать запонять заявку.", reply_markup=keyboard)
 
 async def main():
     dp.include_router(router)
--- handlers.py
+++ handlers.py
@@ -2,45 +2,69 @@
 from aiogram.fsm.state import State, StatesGroup
 from aiogram import Router
 from aiogram.fsm.context import FSMContext
-from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.filters import Command, StateFilter
+from aiogram.types import Message, ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton
 from config import OWNER_ID
+from aiogram import F
+import re
 
 router = Router()
 
+button_main = KeyboardButton(text="📝 Оставить заявку")
+keyboard = ReplyKeyboardMarkup(keyboard=[[button_main]], resize_keyboard=True)
+cancel_button = KeyboardButton(text="❌ Отменить")
+cancel_keyboard = ReplyKeyboardMarkup(keyboard=[[cancel_button]], resize_keyboard=True)
+
+def is_valid_phone(phone: str) -> bool:
+    pattern = r'^\+7[\s\-]?\(?\d{3}\)?[\s\-]?\d{3}[\s\-]?\d{2}[\s\-]?\d{2}$'
+    return bool(re.match(pattern, phone))
 
 class OrderForm(StatesGroup):
     waiting_for_name = State()
     waiting_for_phone = State()
     waiting_for_car_model = State() #по ТЗаданию, нужно добавить поле для модели автомобиля
     waiting_for_problem_description = State()
-    
-    
-@router.message(Command("request"))
-async def cmd_request(message: Message, state: FSMContext):
+
+@router.message(F.text == "❌ Отменить", StateFilter(
+    OrderForm.waiting_for_name,
+    OrderForm.waiting_for_phone,
+    OrderForm.waiting_for_car_model,
+    OrderForm.waiting_for_problem_description
+))
+async def cancel_order(message: Message, state: FSMContext):
+    await state.clear()
+    await message.answer("В следующий раз!", reply_markup=keyboard)
+
+ 
+@router.message(F.text == "📝 Оставить заявку")
+async def btn_request(message: Message, state: FSMContext):
     await state.set_state(OrderForm.waiting_for_name)
-    await message.answer("Как вас зовут?")
+    await message.answer("Как вас зовут?", reply_markup=cancel_keyboard)
     
     
 @router.message(OrderForm.waiting_for_name)
 async def process_name(message: Message, state: FSMContext):
     await state.update_data(name=message.text)  # сохраняем имя
     await state.set_state(OrderForm.waiting_for_phone)
-    await message.answer("Ваш номер телефона?")
+    await message.answer("Ваш номер телефона в формате +7XXXXXXXXXX")
     
 @router.message(OrderForm.waiting_for_phone)
 async def process_phone(message: Message, state: FSMContext):
-    await state.update_data(phone=message.text)  # сохраняем номер телефона
+    if not is_valid_phone(message.text):  # ← сначала проверяем
+        await message.answer("❌ Неверный формат. Введите номер в формате +7XXXXXXXXXX")
+        return  # ← выходим, состояние не меняется
+    
+    await state.update_data(phone=message.text)  # сохраняем только если номер верный
     await state.set_state(OrderForm.waiting_for_car_model)
-    await message.answer("Марка и модель вашего автомобиля?")
+    await message.answer("Марка и модель вашего автомобиля?", reply_markup=cancel_keyboard)
     
 @router.message(OrderForm.waiting_for_car_model)
 async def process_car_model(message: Message, state: FSMContext):
     await state.update_data(car_model=message.text)  # сохраняем модель автомобиля
     await state.set_state(OrderForm.waiting_for_problem_description)
-    await message.answer("Опишите проблему с автомобилем.")
-    
-    
+    await message.answer("Опишите проблему с автомобилем.", reply_markup=cancel_keyboard)
+
+
 @router.message(OrderForm.waiting_for_problem_description)
 async def process_problem(message: Message, state: FSMContext):
     await state.update_data(problem=message.text)
